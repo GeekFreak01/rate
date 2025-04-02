da8021ad37fdc2a1b6b0ae6d2cc9cb6366cf19a5
geekfreak <56112622+GeekFreak01@users.noreply.github.com>
2025-04-02 11:37:49 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
 
