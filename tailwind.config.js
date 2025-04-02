1619daad9049d2947aeab7535a7d768d73ef843a
geekfreak <56112622+GeekFreak01@users.noreply.github.com>
2025-04-02 12:21:09 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
-
