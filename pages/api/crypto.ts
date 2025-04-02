c27fd8e35cd6f29502b4c1730b94ba7a21690cfa
geekfreak <56112622+GeekFreak01@users.noreply.github.com>
2025-04-02 11:35:34 +0500

    Update crypto.ts

--- pages/api/crypto.ts
+++ pages/api/crypto.ts
@@ -1,1 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({
+    btc: 83000,
+    eth: 3200,
+    ton: 5.35,
+    usd: 89.45,
+    eur: 96.23,
+    changes: {
+      btc: 1.42,
+      eth: 0.73,
+      ton: 3.51,
+      usd: 0.2,
+      eur: 0.3,
+    },
+  })
+}
 
